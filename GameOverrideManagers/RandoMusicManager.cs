0ef71805b4612c2eaa145a968ed6c83153b8575c
alwaysintreble <mmmcheese158@gmail.com>
2023-12-04 13:12:14 -0600

    the title song really doesn't like being shuffled

--- GameOverrideManagers/RandoMusicManager.cs
+++ GameOverrideManagers/RandoMusicManager.cs
@@ -48,12 +48,12 @@ namespace MessengerRando.GameOverrideManagers
             Debug.Log("playing music");
             // Debug.Log(audioObjectDefinition.GetInstanceID());
             // Debug.Log(audioObjectDefinition.GetInstanceID());
-            if (audioObjectDefinition.IsMusic() && shuffleMusic)
-            {
-                var newAudio = Manager<AudioManager>.Instance.GetRandomLevelTrack();
-                var dimension = random.Next(0, 2);
-                audioObjectDefinition = dimension == 0 ? newAudio.track_8 : newAudio.track_16;
-            }
+            if (!audioObjectDefinition.IsMusic() || !shuffleMusic ||
+                Manager<LevelManager>.Instance.GetCurrentLevelEnum() == ELevel.NONE)
+                return orig(self, audioObjectDefinition, loop, fadeInDuration, playbackTime, customAudioObject);
+            var newAudio = Manager<AudioManager>.Instance.GetRandomLevelTrack();
+            var dimension = random.Next(0, 2);
+            audioObjectDefinition = dimension == 0 ? newAudio.track_8 : newAudio.track_16;
             return orig(self, audioObjectDefinition, loop, fadeInDuration, playbackTime, customAudioObject);
         }
     }

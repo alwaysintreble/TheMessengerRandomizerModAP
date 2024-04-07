7af66241ea138f9cad5b53c6763b90c910417858
alwaysintreble <mmmcheese158@gmail.com>
2024-04-07 12:44:17 -0500

    cleanup item extensions

--- Utils/NetworkItemExtensions.cs
+++ Utils/NetworkItemExtensions.cs
@@ -18,12 +18,15 @@ namespace MessengerRando.Utils
             var ownPlayer = ArchipelagoClient.Session.ConnectionInfo.Slot;
             if (item.Player.Equals(ownPlayer))
             {
-                text = item.Location.Equals(-1) ? $"Cheated {item.Colorize()}!" : $"Found {item.Colorize()}!";
+                text = $"Found {item.Colorize()}!";
+            }
+            else if (item.Player.Equals(-1))
+            {
+                text = $"Cheated {item.Colorize()}";
             }
             else
             {
-                text = $"Received {item.Colorize()} from <color=#{UserConfig.OtherPlayerColor}>" +
-                       $"{ArchipelagoClient.Session.Players.GetPlayerAlias(item.Player)}</color>!";
+                text = $"Received {item.Colorize()} from {item.PlayerColor()}!";
                 return text;
             }
             return text;
@@ -33,22 +36,57 @@ namespace MessengerRando.Utils
 
         public static string Colorize(this NetworkItem item)
         {
-            var color = UserConfig.FillerColor;
-            switch (item.Flags)
-            {
-                case ItemFlags.Advancement:
-                    color = UserConfig.AdvancementColor;
-                    break;
-                case ItemFlags.NeverExclude:
-                    color = UserConfig.UsefulColor;
-                    break;
-                case ItemFlags.Trap:
-                    color = UserConfig.TrapColor;
-                    break;
-            }
+            var color = item.Color();
             var text = color.IsNullOrEmpty() ? item.Name() : $"<color=#{color}>{item.Name()}</color>";
 
             return text;
+        }
+
+        private static string PlayerColor(this NetworkItem item)
+        {
+            return $"<color=#{UserConfig.OtherPlayerColor}>" +
+                   $"{ArchipelagoClient.Session.Players.GetPlayerAlias(item.Player)}</color>";
+        }
+
+        public static string GetShopDescription(this NetworkItem item)
+        {
+            var description =
+                !ArchipelagoClient.Authenticated ||
+                item.Player.Equals(ArchipelagoClient.Session.ConnectionInfo.Slot)
+                    ? "Huh. How did this get here? "
+                    : $"Looks like {item.PlayerColor()} lost this. ";
+            
+            switch (item.Flags)
+            {
+                case ItemFlags.Advancement:
+                    description += "Seems important.";
+                    break;
+                case ItemFlags.NeverExclude:
+                    description += "Seems useful.";
+                    break;
+                case ItemFlags.Trap:
+                    description += "Seems harmful.";
+                    break;
+                default:
+                    description += "Seems like junk.";
+                    break;
+            }
+            return description;
+        }
+
+        private static string Color(this NetworkItem item)
+        {
+            switch (item.Flags)
+            {
+                case ItemFlags.Advancement:
+                    return UserConfig.AdvancementColor;
+                case ItemFlags.NeverExclude:
+                    return UserConfig.UsefulColor;
+                case ItemFlags.Trap:
+                    return UserConfig.TrapColor;
+                default:
+                    return UserConfig.FillerColor;
+            }
         }
     }
 }
